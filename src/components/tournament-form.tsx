b3a186a3048fdbb3d28e6d2c53f819749066dc95
Kadir <nicolas.ciftci@efrei.net>
2025-05-06 09:23:09 +0200

    first

--- src/components/tournament-form.tsx
+++ src/components/tournament-form.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 interface TournamentFormProps {
